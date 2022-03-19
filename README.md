bc05ec8d99e29bbcfd27568cf6fc9f4b86806f5d
Criz <crizmo595@gmail.com>
2022-03-19 07:55:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <a href="https://www.npmjs.com/package/anyanime">
+    <img src="https://media.discordapp.net/attachments/953273278770860082/954403125584871454/anyanimeapi.png?width=1279&height=196" alt="Any Anime Api">
+  </a>
+</p>
+
+# Any Anime Api
+
+AnyAnime api is a addon on the original anyanime npm package which works similar to the package
+[]: # Language: javascript
+
+The api & package is made by `Criz#1270`
+
+Get a random anime pfp / image per request.
+
+- Reload the site or click the button to get a new image.
+
+# Package
+
+If you want to use the package instead of the api here's the link:
+
+- [AnyAnime](https://www.npmjs.com/package/anyanime/v/1.2.14)
+- [Github](https://github.com/crizmo/AnyAnime)
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
+
+app.get('/anime', (req, res) => {
+    const anime = require("./database/anime.json");
+    const mathRandom = (number) => ~~(Math.random() * number);
+    res.status(200).send({
+        message: 'Hi User',
+        status: 'success',
+        data: {
+            name: 'Anime img source',
+            image: anime[mathRandom(anime.length)]
+        }
+    });
+});
+
+app.get('/anime/img', (req, res) => {
+    const anime = require("./database/anime.json");
+    const AnyAnime = () => {
+        return {
+            anime: () => anime[mathRandom(anime.length)],
+        };
+    };
+    const mathRandom = (number) => ~~(Math.random() * number);
+
+    const daimg = AnyAnime().anime();
+    const image = `<img src="${daimg}" style="height: auto; width: 10%;">`;
+    const reload = `<button onclick="location.reload()">Reload</button>`;
+    res.send(image + reload);
+})
