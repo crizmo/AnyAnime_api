e28598aa7a32a18a1b944a03bddbf05694fe2e9d
Kurizu <83665497+crizmo@users.noreply.github.com>
2022-12-21 17:18:39 +0530

    🔥 : json -> mongodb

--- index.js
+++ index.js
@@ -1,41 +1,74 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+require('./database/connection')
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send(`Hi ! Welcome to AnyAnime Api , Please using the following endpoints :- <br>To get json data - <a href="https://anyanime-api.herokuapp.com/anime">/anime</a> <br>To get random anime img / pfp - <a href="https://anyanime-api.herokuapp.com/anime/img">/anime/img</a>`);
+    res.send(`Hi ! Welcome to AnyAnime Api , Please using the following endpoints :- <br>To get json data - <a href="https://anyanime-api.kurizu.repl.co/anime">/anime</a> <br>To get random anime img / pfp - <a href="https://anyanime-api.kurizu.repl.co/anime/img">/anime/img</a>`);
 })
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
 
+// const anime = require("./database/anime.json");
+// const Anime = require("./database/models/anime");
+// anime.forEach(async (anime) => {
+//     if (anime.endsWith(".png")) {
+//         const imgurl = await Anime.findOne({ type: "png" });
+//         imgurl.urls.push(anime);
+//         await imgurl.save();
+//     } else if (anime.endsWith(".gif")) {
+//         const imgurl = await Anime.findOne({ type: "gif" });
+//         imgurl.urls.push(anime);
+//         await imgurl.save();
+//     }
+// });
+
 app.get('/anime', (req, res) => {
-    const anime = require("./database/anime.json");
     const mathRandom = (number) => ~~(Math.random() * number);
-    res.status(200).send({
-        message: 'Hi User',
-        status: 'success',
-        stuff: [{
-            name: 'Anime img source',
-            image: anime[mathRandom(anime.length)]
-        }]
+
+    const Anime = require("./database/models/anime");
+    Anime.find({ type: "png" }).then((result) => {
+        console.log(result[0].urls.length);
+        res.status(200).send({
+            message: 'Hi User',
+            status: 'success',
+            stuff: [{
+                name: 'Anime img source',
+                image: result[0].urls[mathRandom(result[0].urls.length)]
+            }]
+        });
+    }).catch((err) => {
+        console.log(err);
     });
+
 });
 
 app.get('/anime/img', (req, res) => {
-    const anime = require("./database/anime.json");
-    const AnyAnime = () => {
-        return {
-            anime: () => anime[mathRandom(anime.length)],
-        };
-    };
+    const Anime = require("./database/models/anime");
     const mathRandom = (number) => ~~(Math.random() * number);
 
+    const AnyAnime = () => {    
+        return {
+            anime: () => Anime.find({ type: "png" }).then((result) => {
+                console.log(result[0].urls[mathRandom(result[0].urls.length)])
+                return result[0].urls[mathRandom(result[0].urls.length)];
+            }).catch((err) => {
+                console.log(err);
+            }),
+        };
+    };  
+
     const daimg = AnyAnime().anime();
-    const image = `<img src="${daimg}" style="height: auto; width: 10%;">`;
-    const reload = `<button onclick="location.reload()">Reload</button>`;
-    res.send(image + reload);
+    daimg.then((result) => {
+        const image = `<img src="${result}" style="height: auto; width: 10%;">`;
+        const reload = `<button onclick="location.reload()">Reload</button>`;
+        res.send(image + reload);
+    }
+    ).catch((err) => {
+        console.log(err);
+    });
 })
