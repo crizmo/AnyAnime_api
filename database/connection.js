86dc7e228d030ce55dd9454eb8d5533e3e9f5473
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-01-25 18:53:21 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+mongoose.set('strictQuery', false);
 
 mongoose.connect(`${process.env.DB_URL}`, ()=> {
   console.log('connected to mongodb')
